74f938f390a02a699880a5c5f5a80fde09e67a7e
oyps <ponconsoft@gmail.com>
2023-01-18 01:31:43 +0800

    ..

--- footer.php
+++ footer.php
@@ -1,7 +1,17 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <footer class="text-center pb-5">
-    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-    由 <a href="http://www.typecho.org">Typecho</a> 强力驱动.
+    &copy; <?php echo date('Y'); ?>
+    <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
+    All rights reserved.
+    <div>
+        备案号：<a href="https://beian.miit.gov.cn/" class="text-dark" target="_blank">赣ICP备2022000371号-2</a>
+    </div>
+    <div>
+        <a target="_blank" class="text-dark" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=36010802000693">
+            <img referrerPolicy="no-referrer" src="https://p.ananas.chaoxing.com/star3/origin/6688ed27e29b4feef62cc2518e2e4de3.png" style="height: 1em;" class="align-middle" alt="公安备案">
+            赣公网安备 36010802000693号
+        </a>
+    </div>
 </footer>
 </body>
 
