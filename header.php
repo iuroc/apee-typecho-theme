09d2650b87d71096d8fc91cacc7f1de45c2f0123
oyps <ponconsoft@gmail.com>
2023-01-18 18:40:58 +0800

    新增强制浅色主题

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
             ], '', ' - ');
             $this->options->title(); ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.staticfile.org/github-markdown-css/5.1.0/github-markdown.min.css">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/github-markdown-css/5.1.0/github-markdown-light.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
     <script>
